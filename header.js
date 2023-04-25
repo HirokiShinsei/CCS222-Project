3553ddaf42a42e6b35017c3c91090bd958da9d9a
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2023-04-25 17:04:59 +0800

    Refactored header files

--- header.js
+++ header.js
@@ -1,31 +1,10 @@
-const optionBtn = document.querySelector('#dropdown');
-const optionBox = document.querySelector('.dropdown-box');
-const optionItem = document.querySelectorAll('.dropdown-item');
-const userBtn = document.querySelector('.user');
-const userBox = document.querySelector('.user-dropdown');
+const logoutBtn = document.querySelector('.tab-option[name="logout"]');
 
-optionBtn.onclick = function(e) {
-    optionBox.classList.toggle('dropdown-box-active');
-}
+logoutBtn.addEventListener('click', () => {
+    let xhr = new XMLHttpRequest();
+    
+    xhr.open('GET', 'logout.php');
+    xhr.send();
 
-userBtn.addEventListener('click', e => {
-    userBox.classList.toggle('user-dropdown-active');
-});
-
-window.addEventListener('click', e => {
-    if(!optionBtn.contains(e.target)) {
-        optionBox.classList.remove('dropdown-box-active');
-    }
-    if (!userBtn.contains(e.target)) {
-        userBox.classList.remove('user-dropdown-active');
-    }
-
-    if (logout.contains(e.target)) {
-        let xhr = new XMLHttpRequest();
-
-        xhr.open('GET', 'logout.php');
-        xhr.send();
-
-        window.location.href = "login.php";
-    }
-});
+    window.location.href = 'login.php';
+})
--- header.php
+++ header.php
@@ -1,111 +1,89 @@
-<?php 
-session_start();
+<header>
+    <?php session_start(); ?>
 
-function setDropdowns() {
-    if ($_SERVER['PHP_SELF'] != '/CCS222-Project/home.php') {
-        echo 
-        '
-        <div class="dropdown-item" onclick="window.location.href = \'home.php\';">
-            <img src="img/home.png" alt="" class="icon">
-            <p>Home</p>
-        </div>
-        ';
-    }
-    if ($_SERVER['PHP_SELF'] != '/CCS222-Project/communities.php') {
-        echo 
-        '
-        <div class="dropdown-item" onclick="window.location.href = \'communities.php\';">
-            <img src="img/add.png" alt="" class="icon">
-            <p>Communities</p>
-        </div>
-        ';
-    }
-    if ($_SERVER['PHP_SELF'] != '/CCS222-Project/trending.php') {
-        echo 
-        '
-        <div class="dropdown-item" onclick="window.location.href = \'trending.php\';">
-            <img src="img/trend.png" alt="" class="icon">
-            <p>Trending</p>
-        </div>
-        ';
-    }
-}
-
-function setMainTab() {
-    switch($_SERVER['PHP_SELF']) {
-        case '/CCS222-Project/home.php':
-            echo '<img src="img/home.png" alt="" class="icon"><p>Home</p>';
-            break;
-        case '/CCS222-Project/communities.php':
-            echo '<img src="img/add.png" alt="" class="icon"><p>Communities</p>';
-            break;
-        case '/CCS222-Project/trending.php':
-            echo '<img src="img/trend.png" alt="" class="icon"><p>Trending</p>';
-            break;
-    }
-}
-?>
-
-
-<header id="main-header">
     <h2>DiscussDen</h2>
 
-    <!-- All Tabs -->
-    <div id="dropdown">
+    <!-- Tab Box -->
+    <div id="tab-box">
+        <?php 
+            if ($_SERVER['PHP_SELF'] == '/CCS222-Project/home.php') {
+                echo '
+                <div class="tab-option" onclick="window.location.href=\'home.php\'">
+                    <img src="img/home.png" alt="" class="icon">
+                    <p>Home</p>
+                </div>
+                <div class="tab-option" onclick="window.location.href=\'communities.php\'">
+                    <img src="img/add.png" alt="" class="icon">
+                    <p>Communities</p>
+                </div>
+                <div class="tab-option" onclick="window.location.href=\'trending.php\'">
+                    <img src="img/trend.png" alt="" class="icon">
+                    <p>Trending</p>
+                </div>
+                ';
+            }
+            else if ($_SERVER['PHP_SELF'] == '/CCS222-Project/communities.php') {
+                echo '
+                <div class="tab-option" onclick="window.location.href=\'communities.php\'">
+                    <img src="img/add.png" alt="" class="icon">
+                    <p>Communities</p>
+                </div>
+                <div class="tab-option" onclick="window.location.href=\'home.php\'">
+                    <img src="img/home.png" alt="" class="icon">
+                    <p>Home</p>
+                </div>
+                <div class="tab-option" onclick="window.location.href=\'trending.php\'">
+                    <img src="img/trend.png" alt="" class="icon">
+                    <p>Trending</p>
+                </div>
+                ';
+            }
+            else if ($_SERVER['PHP_SELF'] == '/CCS222-Project/trending.php') {
+                echo '
+                <div class="tab-option" onclick="window.location.href=\'trending.php\'">
+                    <img src="img/trend.png" alt="" class="icon">
+                    <p>Trending</p>
+                </div>
+                <div class="tab-option" onclick="window.location.href=\'home.php\'">
+                    <img src="img/home.png" alt="" class="icon">
+                    <p>Home</p>
+                </div>
+                <div class="tab-option" onclick="window.location.href=\'communities.php\'">
+                    <img src="img/add.png" alt="" class="icon">
+                    <p>Communities</p>
+                </div>
+                ';
+            }
+        ?>
 
-        <!-- Set Main Tab -->
-        <?php setMainTab() ?>
-
-        <!-- Dropdown arrow -->
-        <img src="img/drop down.png" alt="dropdownicon" class="icon">
-
-        <!-- Dropdown box -->
-        <nav class="dropdown-box">
-            <section></section>
-
-            <!-- Dropdown options -->
-            <?php setDropdowns() ?>
-        </nav>
+        <img src="img/drop down.png" alt="" class="dropdown icon">
     </div>
 
     <!-- Search bar -->
-    <svg id="searchbar">
-        <rect x="0" y="0" rx="15" ry="15" width="100%" height="100%" stroke="none" fill="#e4e4e4"/>
-        <image href="img/search.png" x="-22%" y="25%" width="50%" height="50%"/>
-        <foreignObject x="7%" y="0" width=85% height=100%>
-            <form action="" method="post" id="searchtext">
-                <input type="text" name="searchquery" placeholder="Search a post or article">
-            </form>
-        </foreignObject>
-    </svg>
-    
-    <!-- User Icon -->
-    <?php if (isset($_SESSION['username'])) {
-    echo 
-    '
-    <div class="user">
-        <svg width="40" height="40">
-            <circle cx="20" cy="20" r="20" fill="black" />
-        </svg>
-        <p id="firstname">' . $_SESSION['firstname'] . '</p>
-        <p id="username">' . $_SESSION['username'] . '</p>
-        
-        <div class="user-dropdown">
-            <div class="dropdown-item" id="profile">
-                <img src="img/trend.png" alt="" class="icon">
-                <p>Profile</p>
-            </div>
-            <div class="dropdown-item" id="logout">
-                <img src="img/trend.png" alt="" class="icon">
-                <p>Log Out</p>
+    <form action="" method="get" id="searchbar">
+        <img src="img/search.png" alt="" class="icon">
+        <input name="search" type="text" placeholder="Search a post or article">
+    </form>
+
+    <!-- User Button -->
+    <?php if(isset($_SESSION['username'])) {
+        echo '
+        <div id="user-btn">
+            <svg width=50 height=50>
+                <circle cx=25 cy=25 r=25 fill=black />
+            </svg>
+            <p class="firstname">' . $_SESSION['firstname'] . '</p>
+            <p class="username">' . $_SESSION['username'] . '</p>
+
+            <div class="tab-option" name="logout">
+                <img src="img/add.png" alt="" class="icon">
+                <p>Log out</p>
             </div>
         </div>
-    </div>
-    ';
+        ';
     } else {
-        echo
-        '<a href="login.php" id="redirect">Log In or Sign Up</a>';
-    }
-    ?>
+        echo '<a href="login.php">Log In or Sign Up</a>';
+    } ?>
+
+    <script src="header.js"></script>
 </header>
-<script src="header.js"></script>
