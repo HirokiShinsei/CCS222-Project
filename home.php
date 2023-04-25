68ba1f9cbe7d8312bfe06c8ed352152255cfcb3d
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2023-04-25 15:14:24 +0800

    s

--- home.php
+++ home.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="tabs.css">
+    <link rel="stylesheet" href="header.css">
+    
 </head>
 <body>
     <?php include "header.php" ?>
