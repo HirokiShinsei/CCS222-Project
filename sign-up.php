3952335a722cae64d261ca7f9f25574671f1925b
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2023-04-26 11:42:27 +0800

    Fixed github insertion errors

--- sign-up.php
+++ sign-up.php
@@ -65,6 +65,8 @@
                         $insert_data -> execute();
 
                         session_destroy();
+                        setcookie("username", $_SESSION['username'], time() + (86400 * 7), "/");
+                        setcookie("firstname", $_SESSION['firstname'], time() + (86400 * 7), "/");
                         header('Location: login.php');
 
                     }
