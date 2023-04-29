1e021c8404f0927c6fa8113f13cec54ef96ff1b0
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2023-04-29 15:23:14 +0800

    Added quality of life changes

--- header.js
+++ header.js
@@ -1,10 +1,101 @@
+// Logout button
 const logoutBtn = document.querySelector('.tab-option[name="logout"]');
 
-logoutBtn.addEventListener('click', () => {
+logoutBtn.addEventListener('click', logOut);
+logoutBtn.addEventListener('keydown', e => {
+    if (e.key === 'Enter') logOut();
+});
+
+// Drop down options
+const dropdowns = document.querySelectorAll('header > div');
+dropdowns.forEach(div => navigateThrough(div));
+
+// Search box
+const searchInput = document.querySelector('#searchbar > input');
+
+searchInput.addEventListener('input', () => {
+    searchbar.classList.add('start-input');
+});
+
+searchInput.addEventListener('blur', () => {
+    searchbar.classList.remove('start-input');
+})
+
+// Search options
+const searchBox = document.querySelector('#search-box');
+
+searchInput.addEventListener('keydown', e => {
+    if (!searchbar.classList.contains('start-input')) {
+        searchbar.classList.add('start-input');
+    }
+
+    if (e.key === 'ArrowDown') {
+        e.preventDefault();
+        const children = searchBox.querySelectorAll('section');
+        
+        let currentIndex = -1;
+        for (let i = 0; i < children.length; i++) {
+            if (children[i].classList.contains('active')) {
+                currentIndex = i;
+                break;
+            }
+        }
+        const nextIndex = currentIndex === children.length - 1 ? 0 : currentIndex + 1;
+    
+        children[nextIndex].classList.add('active');
+        searchInput.value = children[nextIndex].innerText;
+        if (currentIndex >= 0) children[currentIndex].classList.remove('active');
+    
+    } else if (e.key === 'ArrowUp') {
+        e.preventDefault();
+        const children = searchBox.querySelectorAll('section');
+        
+        let currentIndex = 1;
+        for (let i = 0; i < children.length; i++) {
+            if (children[i].classList.contains('active')) {
+                currentIndex = i;
+                break;
+            }
+        }
+        const nextIndex = currentIndex === 0 ? children.length - 1 : currentIndex - 1;
+    
+        children[nextIndex].classList.add('active')
+        searchInput.value = children[nextIndex].innerText;
+        children[currentIndex].classList.remove('active');
+    }
+});
+
+// -----------------------------------------------
+// FUNCTIONS
+// -----------------------------------------------
+function navigateThrough(div) {
+    div.addEventListener('keydown', e => {
+        if (e.key === 'ArrowDown') {
+            e.preventDefault();
+            const children = div.querySelectorAll('.tab-option');
+            console.log(children);
+            
+            const currentIndex = Array.from(children).indexOf(document.activeElement);
+            const nextIndex = currentIndex === children.length - 1 ? 0 : currentIndex + 1;
+            children[nextIndex].focus();
+            
+        } else if (e.key === 'ArrowUp') {
+            e.preventDefault();
+            const children = div.querySelectorAll('.tab-option');
+            console.log(children);
+            const currentIndex = Array.from(children).indexOf(document.activeElement);
+            const prevIndex = currentIndex === 0 ? children.length - 1 : currentIndex - 1;
+            children[prevIndex].focus();
+        }
+    });
+}
+
+function logOut() {
     let xhr = new XMLHttpRequest();
     
     xhr.open('GET', 'logout.php');
     xhr.send();
 
     window.location.href = 'login.php';
-})
+}
+
--- header.php
+++ header.php
@@ -4,19 +4,19 @@
     <h2>DiscussDen</h2>
 
     <!-- Tab Box -->
-    <div id="tab-box">
+    <div id="tab-box" tabindex=0>
         <?php 
             if ($_SERVER['PHP_SELF'] == '/CCS222-Project/home.php') {
                 echo '
-                <div class="tab-option" first-option>
+                <div class="tab-option" first-option tabindex=-1>
                     <img src="img/home.png" alt="" class="icon">
                     <p>Home</p>
                 </div>
-                <div class="tab-option" onclick="window.location.href=\'communities.php\'">
+                <div class="tab-option" onclick="window.location.href=\'communities.php\'" tabindex=-1>
                     <img src="img/add.png" alt="" class="icon">
                     <p>Communities</p>
                 </div>
-                <div class="tab-option" onclick="window.location.href=\'trending.php\'">
+                <div class="tab-option" onclick="window.location.href=\'trending.php\'" tabindex=-1>
                     <img src="img/trend.png" alt="" class="icon">
                     <p>Trending</p>
                 </div>
@@ -24,15 +24,15 @@
             }
             else if ($_SERVER['PHP_SELF'] == '/CCS222-Project/communities.php') {
                 echo '
-                <div class="tab-option" first-option>
+                <div class="tab-option" first-option tabindex=-1>
                     <img src="img/add.png" alt="" class="icon">
                     <p>Communities</p>
                 </div>
-                <div class="tab-option" onclick="window.location.href=\'home.php\'">
+                <div class="tab-option" onclick="window.location.href=\'home.php\'" tabindex=-1>
                     <img src="img/home.png" alt="" class="icon">
                     <p>Home</p>
                 </div>
-                <div class="tab-option" onclick="window.location.href=\'trending.php\'">
+                <div class="tab-option" onclick="window.location.href=\'trending.php\'" tabindex=-1>
                     <img src="img/trend.png" alt="" class="icon">
                     <p>Trending</p>
                 </div>
@@ -40,15 +40,15 @@
             }
             else if ($_SERVER['PHP_SELF'] == '/CCS222-Project/trending.php') {
                 echo '
-                <div class="tab-option" first-option>
+                <div class="tab-option" first-option tabindex=-1>
                     <img src="img/trend.png" alt="" class="icon">
                     <p>Trending</p>
                 </div>
-                <div class="tab-option" onclick="window.location.href=\'home.php\'">
+                <div class="tab-option" onclick="window.location.href=\'home.php\'" tabindex=-1>
                     <img src="img/home.png" alt="" class="icon">
                     <p>Home</p>
                 </div>
-                <div class="tab-option" onclick="window.location.href=\'communities.php\'">
+                <div class="tab-option" onclick="window.location.href=\'communities.php\'" tabindex=-1>
                     <img src="img/add.png" alt="" class="icon">
                     <p>Communities</p>
                 </div>
@@ -61,28 +61,35 @@
 
     <!-- Search bar -->
     <form action="" method="get" id="searchbar">
+        <!-- Search box -->
+        <div id="search-box">
+            <section>Option 1</section>
+            <section>Option 2</section>
+            <section>Option 3</section>
+        </div>
+        
         <img src="img/search.png" alt="" class="icon">
-        <input name="search" type="text" placeholder="Search a post or article" autocomplete="off">
+        <input name="search" type="search" placeholder="Search a post or article" autocomplete="off" tabindex=0>
     </form>
 
     <!-- User Button -->
     <?php if(isset($_SESSION['username'])) {
         echo '
-        <div id="user-btn">
+        <div id="user-btn" tabindex=0>
             <svg width=50 height=50>
                 <circle cx=25 cy=25 r=25 fill=black />
             </svg>
             <p class="firstname">' . $_SESSION['firstname'] . '</p>
             <p class="username">' . $_SESSION['username'] . '</p>
 
-            <div class="tab-option" name="logout">
+            <div class="tab-option" name="logout" tabindex=-1>
                 <img src="img/add.png" alt="" class="icon">
                 <p>Log out</p>
             </div>
         </div>
         ';
     } else {
-        echo '<a href="login.php">Log In or Sign Up</a>';
+        echo '<a href="login.php" tabindex=3>Log In or Sign Up</a>';
     } ?>
 
     <script src="header.js"></script>
