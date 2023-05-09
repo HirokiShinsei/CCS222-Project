56b920c8374d2de12ab65ffe72ed2b04dced4adf
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2023-05-09 23:52:46 +0800

    Modified like button; updated database

--- post-boxes.js
+++ post-boxes.js
@@ -2,14 +2,15 @@ document.querySelectorAll('.like').forEach(like_button => {
     like_button.addEventListener('click', function() {
         const post_id = this.getAttribute("data-id");
 
+        if (this.querySelector(".like-state").getAttribute("src") == "img/upvote-nofill.png") {
+            this.querySelector(".like-state").src = "img/upvote-filled.png";
+        } else {
+            this.querySelector(".like-state").src = "img/upvote-nofill.png";  
+        }
+         
         const xhr = new XMLHttpRequest();
         xhr.onreadystatechange = () => {
             if (xhr.readyState == 4 && xhr.status === 200) {
-                if (this.querySelector(".like-state").getAttribute("src") == "img/upvote-nofill.png") {
-                    this.querySelector(".like-state").src = "img/upvote-filled.png";
-                } else {
-                    this.querySelector(".like-state").src = "img/upvote-nofill.png";  
-                } 
                 this.querySelector('.likes').innerHTML = xhr.responseText;
             }
         }
