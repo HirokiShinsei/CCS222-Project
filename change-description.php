dc0211437cbb409465ea11008aae25a65a6630bc
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2023-05-12 17:34:34 +0800

    Prevented adding tags to new name and bio

--- change-description.php
+++ change-description.php
@@ -5,9 +5,11 @@ session_start();
 $db_file = __DIR__ . '\forum_database.db';
 $db = new PDO('sqlite:' . $db_file);
 
+$newdescription = strip_tags($_POST['newdescription']);
+
 $stmt = $db -> prepare('UPDATE users SET description = :description WHERE username = :current_user');
 $stmt -> bindParam(':current_user', $_SESSION['username']);
-$stmt -> bindParam(':description', $_POST['newdescription']);
+$stmt -> bindParam(':description', $newdescription);
 $stmt -> execute();
 
 header('Location: profile.php');
--- change-name.php
+++ change-name.php
@@ -5,19 +5,21 @@ session_start();
 $db_file = __DIR__ . '\forum_database.db';
 $db = new PDO('sqlite:' . $db_file);
 
+$newname = strip_tags($_POST['newname']);
+
 $stmt = $db -> prepare('UPDATE users SET username = :user WHERE username = :current_user');
 $stmt -> bindParam(':current_user', $_SESSION['username']);
-$stmt -> bindParam(':user', $_POST['newname']);
+$stmt -> bindParam(':user', $newname);
 $stmt -> execute();
 
 // change every comment to new username
 $stmt = $db -> prepare('UPDATE comments SET name = :name WHERE name = :current_user');
 $stmt -> bindParam(':current_user', $_SESSION['username']);
-$stmt -> bindParam(':name', $_POST['newname']);
+$stmt -> bindParam(':name', $newname);
 $stmt -> execute();
 
 // Change session and cookie to new username
-$_SESSION['username'] = $_POST['newname'];
+$_SESSION['username'] = $newname;
 setcookie("username", $_SESSION['username'], time() + (86400 * 7), "/");
 
 header('Location: profile.php');
