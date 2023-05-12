5bfeb0d7386dada2086bbe0ccaf58f7a7ecf5617
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2023-05-12 18:19:12 +0800

    Removed  post box (when no user is logged in)

--- home.php
+++ home.php
@@ -13,13 +13,18 @@
 <body>
     <?php include "header.php"?>
     <div class="input-section">
-        <div class="post-tab">
-            <svg width=35 height=35>
-                <image href="<?php echo $user_profile ?>" width=100% height=100% clip-path="url(#avatar-clip)" />
-            </svg>
-            <input type="text" placeholder="Create a post" onclick="redirect_to_post()" id="post-link">
-            <img src="img/image.png" alt="" class="icon">
-        </div>
+
+        <?php if (isset($_SESSION['username'])) {
+            echo '
+            <div class="post-tab">
+                <svg width=35 height=35>
+                    <image href="<?php echo $user_profile ?>" width=100% height=100% clip-path="url(#avatar-clip)" />
+                </svg>
+                <input type="text" placeholder="Create a post" onclick="redirect_to_post()" id="post-link">
+                <img src="img/image.png" alt="" class="icon">
+            </div>';
+        }
+        ?>
 
         <!-- /* best-hot-new */ -->
         <div class="post-tab">
